0c49c817b45ebd5eb94956de39698d643f72ddb1
Tito Candra Septio <55591161+titocs@users.noreply.github.com>
2021-03-28 10:09:24 +0700

    Update 263A - Beautiful Matrix.cpp

--- 263A - Beautiful Matrix.cpp
+++ 263A - Beautiful Matrix.cpp
@@ -5,12 +5,11 @@ using namespace std;
 
 int main(){
     int x;
-    for (int i=1; i<=5; ++i){
-        for (int j=1; j<=5; ++j){
+    for(int i=1; i<=5; ++i){
+        for(int j=1; j<=5; ++j){
             cin >> x;
-            if(x == 1){
+            if(x == 1)
                 cout << abs(i - 3) + abs(j - 3) << endl;
-            }
         }
     }
     return 0;
