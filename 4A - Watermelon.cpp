5a429346796b228311bc010e1b63bfad99e00048
Hfn <55591161+titocs@users.noreply.github.com>
2021-08-26 13:53:24 +0700

    Create 4A - Watermelon.cpp

---
+++ 4A - Watermelon.cpp
@@ -0,0 +1,9 @@
+// Link problem: https://codeforces.com/problemset/problem/4/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n; cin >> n;
+    cout << (n % 2 == 0 && n > 2 ? "YES" : "NO");
+}
