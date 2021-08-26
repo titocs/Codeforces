8761a004f9eb9e772e847f735629b132f442ab56
Hfn <55591161+titocs@users.noreply.github.com>
2021-08-26 14:01:20 +0700

    Create 282A - Bit++.cpp

---
+++ 282A - Bit++.cpp
@@ -0,0 +1,17 @@
+// Link problem:
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n; cin >> n;
+    int x = 0;
+    while(n--){
+        string s; cin >> s;
+        if(s[1] == '+')
+            x++;
+        else
+            x--;
+    }
+    cout << x;
+}
