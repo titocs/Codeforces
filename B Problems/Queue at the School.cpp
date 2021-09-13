f633cc557d22a1eb35c5494ac848e8eae65e8f13
Hfn <55591161+titocs@users.noreply.github.com>
2021-09-13 22:42:45 +0700

    Create Queue at the School.cpp

---
+++ B Problems/Queue at the School.cpp
@@ -0,0 +1,18 @@
+// Link problem: https://codeforces.com/problemset/problem/266/B
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, t; cin >> n >> t;
+    string s; cin >> s;
+    while(t--){
+        for(int i=1; i<n; ++i){
+            if(s[i] == 'G' && s[i-1] == 'B'){
+                swap(s[i], s[i-1]);
+                i++;
+            }
+        }
+    }
+    cout << s;
+}
