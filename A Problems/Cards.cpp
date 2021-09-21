cd5aa97bdfc7e5deeb42596f1cd3e787f91ae95b
Hfn <55591161+titocs@users.noreply.github.com>
2021-09-21 14:33:14 +0700

    Create Cards.cpp

---
+++ A Problems/Cards.cpp
@@ -0,0 +1,14 @@
+// Link problem: https://codeforces.com/problemset/problem/1220/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, z = 0, no = 0; cin >> n;
+    string s; cin >> s;
+    sort(s.begin(), s.end());
+    for(int i=0; i<s.length(); ++i){
+        if(s[i] == 'z') cout << "0" << " ";
+        else if(s[i] == 'n') cout << "1" << " ";
+    }
+}
