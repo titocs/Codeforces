45272ee95b4ab5f1561b5d7003c71403fc8f642f
Hfn <55591161+titocs@users.noreply.github.com>
2021-09-23 12:47:56 +0700

    Create Ultra-Fast Mathematician.cpp

---
+++ A Problems/Ultra-Fast Mathematician.cpp
@@ -0,0 +1,20 @@
+// Link problem: https://codeforces.com/problemset/status?my=on
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s1, s2; cin >> s1 >> s2;
+    int temp = 0;
+    string res = "";
+    for(int i=0; i<s2.length(); ++i){
+        if(s1[i] == '1' && s2[i] == '0' || s1[i] == '0' && s2[i] == '1')
+            res += "1";
+        else if(s1[i] == '1' && s2[i] == '1')
+            res += "0";
+        else
+            res += "0";
+        temp++;
+    }
+    cout << res;
+}
