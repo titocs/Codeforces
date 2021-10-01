b54d899b918e97c20dc27fa28a32c94eea4c6cdc
Hfn <55591161+titocs@users.noreply.github.com>
2021-10-01 23:50:06 +0700

    Create Vanya and Fence.cpp

---
+++ A Problems/Vanya and Fence.cpp
@@ -0,0 +1,19 @@
+// Link problem: https://codeforces.com/problemset/problem/677/A?csrf_token=b4bc3bc7635b5929039a65e15e0a3d5d
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, h; cin >> n >> h;
+    int count = 0;
+    vector<int> arr(n);
+    for(int i=0; i<n; ++i){
+        cin >> arr[i];
+    }
+    for(int i=0; i<n; ++i){
+        if(arr[i] <= h)
+            count++;
+        else count += 2;
+    }
+    cout << count;
+}
