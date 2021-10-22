f122b7927ef0c1bb006f42ed761cbe3b41792be9
Hfn <55591161+titocs@users.noreply.github.com>
2021-10-22 22:02:50 +0700

    Create Vus the Cossack and a Contest.cpp

---
+++ A Problems/Vus the Cossack and a Contest.cpp
@@ -0,0 +1,10 @@
+// Link problem: https://codeforces.com/problemset/problem/1186/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, m, k;
+    cin >> n >> m >> k;
+    cout << (m >= n && k >= n ? "YES" : "NO");
+}
